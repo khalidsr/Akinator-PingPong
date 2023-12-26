e943506d5d70e6cc68c15b62d2765d8bd8f4b93f
Said Riyani <sriyani@e2r1p12.1337.ma>
2023-12-26 21:10:33 +0100

    first

---
+++ PingPong/src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Akinator from './akinator'
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Akinator />
+  </React.StrictMode>,
+)
